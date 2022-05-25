a1080d53dfe60c27a4358f2dbcf1c6d441548e89
SucculentFetish <junichirou77@gmail.com>
2022-05-26 01:53:03 +0800

    signup skeleton

---
+++ public/signup.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="icon">
+
+    </div>
+    <div class="signupform">
+        <div class="title">
+
+        </div>
+        <div class="inputs">
+            <form>
+                <input type="email" name="email" id="em">
+                <input type="text" name="contactnum" id="cn">
+                <input type="text" name="address" id="ad">
+                <input type="password" name="pw" id="pw">
+            </form>
+        </div>
+        <button class="signup">SIGN UP</button>
+    </div>
+</body>
+</html>
