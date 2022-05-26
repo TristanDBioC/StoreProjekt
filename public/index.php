6fcddf1d0096bfc6fbe468d2b788303446d4de6a
SucculentFetish <junichirou77@gmail.com>
2022-05-26 15:18:28 +0800

    finished layout + page nav functionality

---
+++ public/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Tinda</title>
+    <link rel="stylesheet" href="stylesheets/font.css">
+    <link rel="stylesheet" href="stylesheets/index.css">
+    <link rel="stylesheet" href="stylesheets/dropdown.css">
+</head>
+<body>
+    <div class="header">
+        <div class="icon">
+            <img src="resources/images/3.png" alt="Tinda" style="">
+        </div>
+        <div class="hcatscont">
+            <div class="hcats">
+                <p class="drop">Categories</p>
+                <div class="dropcont">
+                    <a href="#">Men</a>
+                    <a href="#">Women</a>
+                    <a href="#">Children</a>
+                </div>
+            </div>
+        </div>
+        <div class="SoT">
+            <a href="#">Sell on Tinda</a>
+        </div>
+        <div class="searchbar">
+            <form>
+            <input type="text" placeholder="  Look for great finds on Tinda!">
+            <button type="submit" class="search"><img src="resources/images/search.png" alt=""></button>
+            </form>
+        </div>
+        <div class="cart">
+            <a href=#><img src="resources/images/cart.png" alt="Cart" class="carti"><a>
+        </div>   
+        <div class="notifc">
+            <div class="notifs">
+                <img src="resources/images/notifs.png" alt="Notifications" class="notifi">
+                <p class="notifp">Notifications</p>
+            </div>
+        </div>
+        <div class="accs">
+            <a href="signup.php"class="su">Sign Up</a> | <a href="signin.php" class="si">Sign In</a>
+        </div>
+    </div>
+
+    <div class="body">
+        <div class="title">
+            <h1>Categories</h1>
+        </div>
+        <div class="bcats">
+            <div class="women">
+                <img src="resources/images/category - women.png" alt="Women">
+            </div>
+            <div class="men">
+                <img src="resources/images/category - men.png" alt="Men">
+            </div>
+            <div class="children">
+                <img src="resources/images/category - children.png" alt="Children">
+            </div>
+        </div>
+    </div>
+
+    <div class="newsletter">
+        <div class="divider">
+
+        </div>
+        <h1 class="subbox"><a href="newsletter.php" class="sub">Subscribe to our Newsletter</a></h1>
+    </div>
+</body>
+</html>
--- public/signin.php
+++ public/signin.php
@@ -4,27 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
+    <title>Sign In</title>
     <link rel="stylesheet" href="stylesheets/font.css">
     <link rel="stylesheet" href="stylesheets/bg-icon.css">
-    <link rel="stylesheet" href="stylesheets/signup.css">
+    <link rel="stylesheet" href="stylesheets/signin.css">
 </head>
 <body>
     <div class="wrap">
     <div class="icon">
         <img src="resources/images/6.png" alt="Tinda Logo">
     </div>
-    <div class="signupform">
-            <h1 class="title">Sign Up</h1>
+    <div class="signinform">
+            <h1 class="title">Sign In</h1>
         <div class="inputs">
             <form>
                 <input type="email" name="email" id="em" placeholder="  Email">
-                <input type="text" name="contactnum" id="cn" placeholder="  Contact Number">
-                <input type="text" name="address" id="ad" placeholder="  Address">
                 <input type="password" name="pw" id="pw" placeholder="  Password">
             </form>
         </div>
-        <button class="signup">SIGN UP</button>
+        <button class="signin">Sign In</button>
+
+        <p class="new">New to Tinda? <a href="signup.php" class="signup">Sign Up</a></p>
     </div>
     </div>
 </body>
