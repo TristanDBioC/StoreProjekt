99288c1ed35bef02f6f0903ec56b647e6707cb0f
Chem <david.tristankyle@gmail.com>
2022-05-26 20:02:36 +0800

    sign up and login

--- public/signup.php
+++ public/signup.php
@@ -14,14 +14,70 @@
 
         </div>
         <div class="inputs">
-            <form>
-                <input type="email" name="email" id="em">
-                <input type="text" name="contactnum" id="cn">
-                <input type="text" name="address" id="ad">
-                <input type="password" name="pw" id="pw">
+            <form action="" method="POST">
+                Username: <input type="text" name="username" id="em"> <br>
+                Contact: <input type="text" name="contactnum" id="cn"> <br>
+                Address: <input type="text" name="address" id="ad"> <br>
+                Password: <input type="password" name="password" id="pw"> <br>
+                <input type='submit' name='submit'>
             </form>
         </div>
-        <button class="signup">SIGN UP</button>
     </div>
+
+
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            verifyUser();
+        }
+        function verifyUser() {
+            $conn = mysqli_connect('localhost', 'cs36', '1234', 'tindadb');
+            $username = $_POST['username'];
+            $sql = "SELECT * FROM member WHERE username='". $username. "'";
+            $result = mysqli_query($conn, $sql);
+            $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+            if (count($users) == 0) {
+                addtoDB();
+
+            } else {
+                echo 'alert("Username already exists ")';
+            }
+        }
+
+        function addtoDB() {
+            echo "adding to db";
+            $conn = mysqli_connect('localhost', 'cs36', '1234', 'tindadb');
+            // set varuabkes
+            $id = 0;
+            $username = $_POST['username'];
+            $pass = hash('sha256', $_POST['password'], false);
+            $address = $_POST['address'];
+            $contact = $_POST['contactnum'];
+            $displayname = $username;
+            $isseller = 0;
+
+            // SQL statment
+            $sql = "INSERT INTO member VALUES ('".
+                                $id."','".
+                                $username."','".
+                                $pass."','".
+                                $address."','".
+                                $contact."','".
+                                $displayname."',".
+                                $isseller.",'')";
+
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+                echo "Successfully added to DB";
+                // Move to next page
+            } else {
+                echo "Failed";
+                // do something
+            }
+
+        }
+
+    ?>
+
 </body>
 </html>
